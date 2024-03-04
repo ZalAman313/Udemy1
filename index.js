b7020bbda809fa987c63f0b6c03b212698f3cce3
ZalAman313 <dazalaman313@gmail.com>
2024-03-04 17:05:11 -0500

    Commit React app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5001
+
+app.get('/', (req, res) => {
+  res.send({'message_type':'start_guidance'}); 
+  console.log('responsed get request...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.listen(5000);
