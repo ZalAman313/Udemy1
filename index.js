549a19f9e61b338a5174871372832cd74097e715
ZalAman313 <dazalaman313@gmail.com>
2024-03-07 17:59:38 -0500

    display user's information

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const requestIp = require('request-ip');
+const geoip = require('geoip-lite');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send({'message_type':'start_guidance'}); 
+  const ip = requestIp.getClientIp(req);
+  const geo = geoip.lookup(ip);
+
+  res.json({
+    ip: ip,
+    country: geo.country,
+    city: geo.city,
+    region: geo.region,
+    timezone: geo.timezone
+  });
   console.log('responsed get request...');
 });
 
