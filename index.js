5a7a8e705a2821649a35b8a310d307a4bb45187e
ZalAman313 <dazalaman313@gmail.com>
2024-03-07 17:40:29 -0500

    Remove double call to listen function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send({'message_type':'start_guidance'}); 
@@ -12,5 +12,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.listen(PORT);
